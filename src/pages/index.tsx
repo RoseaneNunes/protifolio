53fe5bea6ec468f5f77f96408c76a58a802a05e8
RoseaneNunes <roseane.anjos@aluno.uepb.edu.br>
2025-08-19 19:16:49 -0300

    feat: criar elemento principal da primeira pagina conetendo um pequeno texto introdutorio e umas imagens e elementos graficos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,115 +1,36 @@
 import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+import arame from '../../public/arame.svg'
+import manchaDeTinta from '../../public/manchaDeTinta.svg'
+import { Header } from "@/components/header";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function Home() {
   return (
-    <div
-      className={`${geistSans.className} ${geistMono.className} font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20`}
-    >
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <section className="w-screen h-screen bg-[#D8D8D8] flex  items-center justify-between text-black">
+        <Image src={arame} alt="arame" className="h-screen  z-20" />
+        <div className="absolute hover:animate-[folhear_1s_ease-in-out_forwards] origin-center rotate-  top-20 right-20 border-l-transparent border-r-transparent border-t-amber-500 border-t-[80px] border-l-[50px]   "></div>
+       
+      <div className="w-[903px] left-5 h-screen bg-[url('/folha.svg')] absolute z-10  flex items-center"></div>
+      <Header/>
+      <div className="z-10 ">
+        <div className=" bg-[url('/tiraDePapel.svg')] bg-center h-[108px] w-[531px] pl-5"  >
+          <strong className="">
+            <h2 className="text-[22px]">Ola, Sou</h2>
+            <h1 className="text-[38px] ">Roseane Nunes Dos Anjos</h1>
+          </strong>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <h1 className=" w-[536px] text-[30px] text-black  pt-40">
+          Sou Roseane Nunes, pernambucana, 20 anos, apaixonada por tecnologia
+          desde pequena, quando meu pai se tornou proprietário de uma lan house.
+          Atualmente faço parte da knex consultoria jr, onde ocupo o cargo de
+          desenvolvedora Frontend.
+        </h1>
+      </div>
+      <div className="relative w-[900px] h-[649px] grid place-content-center  ">
+      <Image src={manchaDeTinta} alt="teste" className="absolute z-0"/>
+        <Image  src="https://github.com/RoseaneNunes.png"  alt="imagem de perfil" width={483} height={483} className="rounded-full z-10"/>
+      </div>
+      
+    </section>
   );
 }
